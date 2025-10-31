dacf69a58d33d8c773962a27c2693ab2251ed098
Zach Harel <zach@zharel.me>
2025-10-31 14:59:54 -0400

    Refactor RobotContainer to use new SwerveTrajectoryBuilder and update trajectory following command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,21 +11,17 @@ import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.trajectory.SwerveTrajectory;
 import edu.wpi.first.wpilibj.PS4Controller.Button;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.Constants.AutoConstants;
 import frc.robot.Constants.DriveConstants;
 import frc.robot.Constants.OIConstants;
-import frc.robot.commands.FollowTrajectoryCommand;
 import frc.robot.subsystems.DriveSubsystem;
-import gay.zharel.hermes.profiles.ProfileParams;
-import gay.zharel.hermes.trajectories.TrajectoryBuilder;
-import gay.zharel.hermes.trajectories.TrajectoryBuilderParams;
-import gay.zharel.hermes.trajectory.HermesTrajectory;
-import gay.zharel.hermes.trajectory.SwerveKinematicsConstraints;
+import gay.zharel.hermes.wpitrajectories.SwerveDriveConstraint;
+import gay.zharel.hermes.wpitrajectories.SwerveTrajectoryBuilder;
+import org.wpilib.commands3.Command;
+import org.wpilib.commands3.Scheduler;
+import org.wpilib.commands3.button.JoystickButton;
 
-import static gay.zharel.hermes.trajectory.ConversionsKt.hermes;
+import static edu.wpi.first.units.Units.*;
 
 /*
  * This class is where the bulk of the robot should be declared.  Since Command-based is a
@@ -47,17 +43,20 @@ public class RobotContainer {
     // Configure the button bindings
     configureButtonBindings();
 
+    Scheduler.getDefault().addPeriodic(robotDrive::periodic);
+
     // Configure default commands
     robotDrive.setDefaultCommand(
         // The left stick controls translation of the robot.
         // Turning is controlled by the X axis of the right stick.
-        new RunCommand(
+        robotDrive.runRepeatedly(
             () -> robotDrive.drive(
                 -MathUtil.applyDeadband(driverController.getLeftY(), OIConstants.DRIVE_DEADBAND),
                 -MathUtil.applyDeadband(driverController.getLeftX(), OIConstants.DRIVE_DEADBAND),
                 -MathUtil.applyDeadband(driverController.getRightX(), OIConstants.DRIVE_DEADBAND),
-                true),
-                robotDrive));
+                true)
+        ).named("Gamepad Drive")
+    );
   }
 
   /**
@@ -71,9 +70,7 @@ public class RobotContainer {
    */
   private void configureButtonBindings() {
     new JoystickButton(driverController, Button.kR1.value)
-        .whileTrue(new RunCommand(
-                robotDrive::setX,
-                robotDrive));
+        .whileTrue(robotDrive.runRepeatedly(robotDrive::setX).named("X formation"));
   }
 
   /**
@@ -82,37 +79,25 @@ public class RobotContainer {
    * @return the command to run in autonomous
    */
   public Command getAutonomousCommand() {
-    // Create parameters for trajectory
-    TrajectoryBuilderParams params = new TrajectoryBuilderParams(
-            1e-6,
-            new ProfileParams(0.25, Math.PI/8, 1e-6)
-    );
-
     // Create constraints for trajectory
-    SwerveKinematicsConstraints constraints = new SwerveKinematicsConstraints(
+    SwerveDriveConstraint constraints = new SwerveDriveConstraint(
+            DriveConstants.DRIVE_KINEMATICS.getModules(),
+            AutoConstants.FEEDFORWARD,
+            MetersPerSecond.of(DriveConstants.MAX_SPEED_METERS_PER_SECOND),
+            Volts.of(DriveConstants.NOMINAL_VOLTAGE)
+    );
+
+    // An example trajectory to follow.  All units in meters.
+    SwerveTrajectoryBuilder builder = new SwerveTrajectoryBuilder(
             DriveConstants.DRIVE_KINEMATICS,
-            AutoConstants.MAX_SPEED_METERS_PER_SECOND,
-            AutoConstants.MAX_ACCELERATION_METERS_PER_SECOND_SQUARED
-    );
+            constraints,
+            Pose2d.kZero
+    ).bezierTo(new Translation2d(1, 1), new Translation2d(2, -1))
+            .splineTo(new Translation2d(3, 0), new Rotation2d(0));
 
-    TrajectoryBuilder builder = new TrajectoryBuilder(
-            params,
-            hermes(Pose2d.kZero),
-            0.0,
-            constraints.getVelConstraint(),
-            constraints.getAccelConstraint()
-    );
+    SwerveTrajectory trajectory = builder.build();
 
-      // An example trajectory to follow. All units in meters.
-    SwerveTrajectory trajectory = HermesTrajectory.fromTimeTrajectory(
-            builder.bezierTo(hermes(new Translation2d(1, 1)), hermes(new Translation2d(2, -1)))
-                    .splineTo(hermes(new Translation2d(3, 0)), hermes(new Rotation2d(0)))
-                    .build().wrtTime()
-    ).toSwerveTrajectory(DriveConstants.DRIVE_KINEMATICS);
-
-    FollowTrajectoryCommand trajectoryCommand = new FollowTrajectoryCommand(robotDrive, trajectory);
-
-    // Run path following command, then stop at the end.
-    return trajectoryCommand.andThen(() -> robotDrive.drive(0, 0, 0, false));
+    // Run trajectory following command
+    return robotDrive.followTrajectoryCommand(trajectory);
   }
 }
